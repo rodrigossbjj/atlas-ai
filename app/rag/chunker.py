15c3fc76267574156e6f0298e711f070a98ff080
Rodrigo Sousa <rodrigosousasales9@gmail.com>
2026-06-06 00:59:20 -0300

    feat: implement document chunking strategy

---
+++ app/rag/chunker.py
@@ -0,0 +1,26 @@
+CHUNK_SIZE = 20
+OVERLAP = 200
+
+
+def create_chunks(document: dict) -> list[dict]:
+    chunks = []
+    chunk_id = 1
+
+    for page in document["pages"]:
+        words = page["content"].spltit()
+        start = 0
+
+        while start < len(words):
+            end = min(start + CHUNK_SIZE, len(words))
+
+            chunks.append({
+                "chunk_id": chunk_id,
+                "page": page["page"],
+                "source": document["filename"],
+                "content": " ".join(words[start:end])
+            })
+
+            chunk_id += 1
+            start += CHUNK_SIZE - OVERLAP
+
+    return chunks
